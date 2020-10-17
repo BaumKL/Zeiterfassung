93cc1641bb2c57dee364daa59e8186be71a997cf
Falk Spehle <falkspehle@icloud.com>
2020-10-17 20:06:38 +0200

    Variablen in den html Datien angepast.

--- static/pause.js
+++ static/pause.js
@@ -1,13 +1,12 @@
-function eater(){
+function eat_on(){
     document.getElementById("eat").style = "display:block";
                  }
-function eaterde(){
+function eat_off(){
     document.getElementById("eat").style = "display:none";
                  }
 function convert() {
- if (window.confirm(" Diese funktion darf nur ausgeführ werden wenn alle Mitarbeite ein oder ausgelockt sind !!!!")) { 
+ if (window.confirm(" Diese funktion darf nur ausgeführ werden wenn alle Mitarbeite ein oder ausgelockt sind !!!!")) {
     window.location.href = "/convert";
 }
 
 }
-
