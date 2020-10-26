d5ff16e18d46bbced17aee73fae8646500acc651
BaumKL <falkspehle@icloud.com>
2020-10-26 16:34:28 +0100

    Datei Name verändert !!

--- static/javascript.js
+++ static/javascript.js
@@ -5,7 +5,7 @@ function eat_off(){
     document.getElementById("eat").style = "display:none";
                  }
 function convert() {
- if (window.confirm(" Diese funktion darf nur ausgeführ werden wenn alle Mitarbeite ein oder ausgelockt sind !!!!")) {
+ if (window.confirm(" Diese funktion darf nur ausgeführ werden wenn alle Mitarbeite ausgelockt sind !")) {
     window.location.href = "/convert";
 }
 
