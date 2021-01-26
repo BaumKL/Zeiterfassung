1f72c03a42dacdafb644ba71358d23aa54516033
Falk Spehle <falkspehle@icloud.com>
2021-01-26 08:08:32 +0100

    Projekt aktualisirt

--- main.py
+++ main.py
@@ -299,5 +299,5 @@ def reload(para):
     subprocess.call ( ['sudo','reboot'])
     return redirect('/')
 
-if __name__ == '__main__': #if __name__ == '__main__':
+if __name__ == '__main__':
     app.run ( port=5000 , debug=True ) #app.run(debug=True, port=80, host='0.0.0.0')
